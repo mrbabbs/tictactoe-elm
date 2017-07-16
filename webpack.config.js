0ebf6f41d33b060e0a0c5b7e93c8baf3f8e59cba
mrbabbs <giovanni.esposito@shore.com>
2017-07-16 21:49:22 +0200

    Make build

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,33 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const PRODUCTION = 'PRODUCTION';
+const environment = process.env.ENV || PRODUCTION
+const isProd = environment === PRODUCTION
+const elmLoader = [{
+  loader: 'elm-webpack-loader',
+  options: {
+    debug: !isProd,
+  }
+}];
+
+const cssExtract =new ExtractTextPlugin('bundle.css');
+const elmLoaderDev = [{ loader: 'elm-hot-loader' }, ...elmLoader];
+
+const cssLoader = cssExtract.extract({
+  fallback: 'style-loader',
+  use: [
+    'css-loader',
+    'elm-css-webpack-loader'
+  ]
+});
+const cssLoaderDev = [
+  'style-loader',
+  'css-loader',
+  'elm-css-webpack-loader'
+];
+
+
 
 module.exports = {
   entry: {
@@ -6,13 +35,14 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, './build'),
-    publicPath: "/assets/",
+    publicPath: "",
     filename: "bundle.js"
   },
   resolve: {
     extensions: ['.js', '.elm'],
     modules: ['node_modules']
   },
+  plugins: [cssExtract],
   module: {
     rules: [
       {
@@ -27,21 +57,10 @@ module.exports = {
       }, {
         test: /\.elm$/,
         exclude: [/elm-stuff/, /node_modules/, /Stylesheets\.elm$/],
-        use: [{
-          loader: 'elm-hot-loader'
-        }, {
-          loader: 'elm-webpack-loader',
-          options: {
-            debug: true,
-          }
-        }]
+        use: isProd? elmLoader : elmLoaderDev
       }, {
         test: /Stylesheets\.elm$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'elm-css-webpack-loader'
-        ]
+        use: isProd? cssLoader : cssLoaderDev
       }
     ]
   },
